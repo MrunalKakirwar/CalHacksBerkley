4e9ff957220dbc7efddfb6776020d6214d8efb85
csuftitan <csuftitan@MacBook-Air-401.local>
2024-12-17 17:49:10 -0800

    pushed

---
+++ park-eazy-frontend/src/parkingdata.js
@@ -0,0 +1,342 @@
+export const parkingData = [
+    {
+      "parkingName": "Alice's Spot",
+      "location": "Shattuck Ave, Berkeley",
+      "price": 8,
+      "id": "12345A",
+      "available": false,
+      "fromDate": {
+        "$date": "2024-10-20T08:00:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-20T18:00:00.000Z"
+      },
+      "rating": 4.5,
+      "_id": {
+        "$oid": "670eda4897f561f94f555ded"
+      }
+    },
+    {
+      "parkingName": "Bob's Driveway",
+      "location": "Oxford St, Berkeley",
+      "price": 10,
+      "id": "78967B",
+      "available": true,
+      "fromDate": {
+        "$date": "2024-10-22T09:00:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-22T18:00:00.000Z"
+      },
+      "rating": 4.8,
+      "_id": {
+        "$oid": "671091857f6f8461f6e3e644"
+      }
+    },
+    {
+      "parkingName": "Dave's Lot",
+      "location": "Nutwood Ave, Fullerton",
+      "price": 9,
+      "id": "56129C",
+      "available": true,
+      "fromDate": {
+        "$date": "2024-10-18T12:00:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-18T20:00:00.000Z"
+      },
+      "rating": 4.2,
+      "_id": {
+        "$oid": "671091867f6f8461f6e3e645"
+      }
+    },
+    {
+      "parkingName": "Emma's Garage",
+      "location": "College Ave, Berkeley",
+      "price": 15,
+      "id": "45231D",
+      "available": false,
+      "fromDate": {
+        "$date": "2024-10-25T10:00:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-25T17:00:00.000Z"
+      },
+      "rating": 5.0,
+      "_id": {
+        "$oid": "671091877f6f8461f6e3e646"
+      }
+    },
+    {
+      "parkingName": "Harry's Corner",
+      "location": "Westwood Ave, Fullerton",
+      "price": 12,
+      "id": "98123E",
+      "available": true,
+      "fromDate": {
+        "$date": "2024-10-19T13:00:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-19T18:00:00.000Z"
+      },
+      "rating": 4.6,
+      "_id": {
+        "$oid": "671091887f6f8461f6e3e647"
+      }
+    },
+    {
+      "parkingName": "Grace's Spot",
+      "location": "Telegraph Ave, Berkeley",
+      "price": 7,
+      "id": "34567F",
+      "available": true,
+      "fromDate": {
+        "$date": "2024-10-16T08:00:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-16T19:00:00.000Z"
+      },
+      "rating": 4.7,
+      "_id": {
+        "$oid": "671091897f6f8461f6e3e648"
+      }
+    },
+    {
+      "parkingName": "Carol's Downtown",
+      "location": "Center St, Berkeley",
+      "price": 14,
+      "id": "67543G",
+      "available": false,
+      "fromDate": {
+        "$date": "2024-10-17T10:00:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-17T16:00:00.000Z"
+      },
+      "rating": 4.9,
+      "_id": {
+        "$oid": "671091907f6f8461f6e3e649"
+      }
+    },
+    {
+      "parkingName": "Bob's Garage",
+      "location": "Oxford St, Fullerton",
+      "price": 11,
+      "id": "56342H",
+      "available": true,
+      "fromDate": {
+        "$date": "2024-10-21T12:00:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-21T20:00:00.000Z"
+      },
+      "rating": 4.4,
+      "_id": {
+        "$oid": "671091917f6f8461f6e3e650"
+      }
+    },
+    {
+      "parkingName": "Frank's Lot",
+      "location": "Bancroft Way, Berkeley",
+      "price": 6,
+      "id": "48329I",
+      "available": false,
+      "fromDate": {
+        "$date": "2024-10-22T08:00:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-22T15:00:00.000Z"
+      },
+      "rating": 4.3,
+      "_id": {
+        "$oid": "671091927f6f8461f6e3e651"
+      }
+    },
+    {
+      "parkingName": "Alice's Extra Spot",
+      "location": "Durant Ave, Berkeley",
+      "price": 9,
+      "id": "39284J",
+      "available": true,
+      "fromDate": {
+        "$date": "2024-10-20T07:00:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-20T19:00:00.000Z"
+      },
+      "rating": 4.5,
+      "_id": {
+        "$oid": "671091937f6f8461f6e3e652"
+      }
+    },
+    {
+      "parkingName": "Manan's Parking",
+      "location": "2404 Nutwood Ave, Fullerton",
+      "price": 10,
+      "id": "163K",
+      "available": true,
+      "fromDate": {
+        "$date": "2024-10-11T21:11:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-12T21:09:00.000Z"
+      },
+      "rating": 3.7,
+      "_id": {
+        "$oid": "671091823f6f8461f6e3e62c"
+      }
+    },
+    {
+      "parkingName": "Helen's Spot",
+      "location": "Hearst Ave, Berkeley",
+      "price": 13,
+      "id": "98765L",
+      "available": false,
+      "fromDate": {
+        "$date": "2024-10-16T09:00:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-16T19:00:00.000Z"
+      },
+      "rating": 4.8,
+      "_id": {
+        "$oid": "671091947f6f8461f6e3e653"
+      }
+    },
+    {
+      "parkingName": "John's Corner",
+      "location": "Bancroft Ave, Fullerton",
+      "price": 11,
+      "id": "76890M",
+      "available": true,
+      "fromDate": {
+        "$date": "2024-10-19T13:00:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-19T18:00:00.000Z"
+      },
+      "rating": 4.6,
+      "_id": {
+        "$oid": "671091957f6f8461f6e3e654"
+      }
+    },
+    {
+      "parkingName": "Backup Lot",
+      "location": "University Ave, Berkeley",
+      "price": 8,
+      "id": "13579N",
+      "available": true,
+      "fromDate": {
+        "$date": "2024-10-20T11:00:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-20T16:00:00.000Z"
+      },
+      "rating": 4.1,
+      "_id": {
+        "$oid": "671091967f6f8461f6e3e655"
+      }
+    },
+    {
+      "parkingName": "Lydia's Lot",
+      "location": "Haste St, Berkeley",
+      "price": 7,
+      "id": "75318O",
+      "available": false,
+      "fromDate": {
+        "$date": "2024-10-21T08:00:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-21T18:00:00.000Z"
+      },
+      "rating": 4.2,
+      "_id": {
+        "$oid": "671091977f6f8461f6e3e656"
+      }
+    },
+    {
+      "parkingName": "Peter's Garage",
+      "location": "University Dr, Fullerton",
+      "price": 14,
+      "id": "47568P",
+      "available": true,
+      "fromDate": {
+        "$date": "2024-10-18T09:00:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-18T18:00:00.000Z"
+      },
+      "rating": 4.7,
+      "_id": {
+        "$oid": "671091987f6f8461f6e3e657"
+      }
+    },
+    {
+      "parkingName": "Ryan's Lot",
+      "location": "Milvia St, Berkeley",
+      "price": 9,
+      "id": "28364Q",
+      "available": true,
+      "fromDate": {
+        "$date": "2024-10-19T10:00:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-19T17:00:00.000Z"
+      },
+      "rating": 4.0,
+      "_id": {
+        "$oid": "671091997f6f8461f6e3e658"
+      }
+    },
+    {
+      "parkingName": "Zara's Driveway",
+      "location": "Durant Ave, Berkeley",
+      "price": 12,
+      "id": "90741R",
+      "available": false,
+      "fromDate": {
+        "$date": "2024-10-21T08:00:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-21T19:00:00.000Z"
+      },
+      "rating": 4.9,
+      "_id": {
+        "$oid": "671091707f6f8461f6e3e659"
+      }
+    },
+    {
+      "parkingName": "Paul's Spot",
+      "location": "Bancroft Way, Berkeley",
+      "price": 10,
+      "id": "25416S",
+      "available": true,
+      "fromDate": {
+        "$date": "2024-10-22T09:00:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-22T18:00:00.000Z"
+      },
+      "rating": 4.4,
+      "_id": {
+        "$oid": "671091057f6f8461f6e3e660"
+      }
+    },
+    {
+      "parkingName": "Mark's Space",
+      "location": "Center St, Berkeley",
+      "price": 13,
+      "id": "89231T",
+      "available": false,
+      "fromDate": {
+        "$date": "2024-10-23T10:00:00.000Z"
+      },
+      "toDate": {
+        "$date": "2024-10-23T16:00:00.000Z"
+      },
+      "rating": 4.8,
+      "_id": {
+        "$oid": "671091167f6f8461f6e3e661"
+      }
+    }
+  ]
